30be9e84c6d577d95f18bd9c93b0027490ab7f82
delphvr <delphine.vanrossum@student.uclouvain.be>
2023-03-14 13:41:28 +0100

    fichier code python pour analyse des paquets

---
+++ analyse python/main.py
@@ -0,0 +1,3 @@
+import pyshark
+
+pcap = pyshark.FileCapture("C:\\Users\delph\OneDrive\Documents\\UCL\B3Q2\LINFO1341 - Computer networks  information transfer\Projet\projet_reseaux_1\paquet test 1mess\envoye_1mess.pcapng")
