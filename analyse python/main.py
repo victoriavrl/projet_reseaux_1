e614c8deb457993b389789a492bc80366181d0e1
delphvr <delphine.vanrossum@student.uclouvain.be>
2023-03-16 12:48:26 +0100

    continuation recup info dns

--- analyse python/main.py
+++ analyse python/main.py
@@ -1,36 +1,28 @@
 import pyshark
 
-capture = pyshark.FileCapture("C:\\Users\delph\OneDrive\Documents\\UCL\B3Q2\LINFO1341 - Computer networks  information transfer\Projet\projet_reseaux_1\paquet test 1mess\envoye_1mess.pcapng")
+capture = pyshark.FileCapture("C:\\Users\delph\OneDrive\Documents\\UCL\B3Q2\LINFO1341 - Computer networks  information transfer\Projet\projet_reseaux_1\paquet test 1mess\envoye_1mess_4G.pcapng")
+
+#print(dir(capture))
 
 capture.load_packets()
+
 #https://stackoverflow.com/questions/41417235/pyshark-attribute-error-while-printing-dns-info
 for i in range(len(capture)):
     try :
         #print("t'es ou")
-        print()
         if capture[i].dns.qry_name :
-            print("DNS Request from ip source : ")
-            print(capture[i].dns.qry_nam)
-            print("DNS Request from ip source : ", capture[i].ip.scr)
+            print()
+            print("DNS Request from ip source : ", capture[i].ip.src)
             print("DNS query name : ", capture[i].dns.qry_name)
     except :
         #ignore captures that aren't DNS Request
         continue
     try:
+        print(capture[i].dns.resp_name)
         if capture[i].dns.resp_name:
-            print("DNS Respond from ip source : ", capture[i].ip.scr)
-            print("DNS respond name")
+            print("DNS Respond from ip source : ", capture[i].ip.src)
+            print("DNS respond name : ", capture[i].dns.resp_name) #ce qu'il a demander
     except :
         continue
+    #TODO : chercher domaine name
 
-#print(dir(capture))
-#print(capture["tcp"])
-#for i in range(len(capture)):
-#    print("ici")
-#    print(capture[i].dns.qry_name)
-
-#print(capture[52])
-#print(capture[52].dns.qry_name)
-#print(len(capture))
-#print(capture[0])
-#print(capture.dns.field_names)
