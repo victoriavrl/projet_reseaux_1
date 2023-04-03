aa4c14c20da6b5ead9785f5677441f22d93eea12
delphvr <delphine.vanrossum@student.uclouvain.be>
2023-04-03 09:27:19 +0200

    code pour avoir les ip adresse des communication TCP

---
+++ analyse python/analyseTCP.py
@@ -0,0 +1,34 @@
+import pyshark
+
+def get_TCP_dest(filepath):
+    capture = pyshark.FileCapture(filepath)
+
+    capture.load_packets()
+
+    dest = []
+
+    for p in capture:
+        if hasattr(p, 'tcp'):
+            ip = p.ip.dst
+            isnot = True
+            for e in dest :
+                if e == ip:
+                    isnot= False
+                    break
+            if (isnot):
+                dest.append(ip)
+
+            ip = p.ip.src
+            isnot = True
+            for e in dest:
+                if e == ip:
+                    isnot = False
+                    break
+            if (isnot):
+                dest.append(ip)
+    return dest
+
+
+filepath = "C:\\Users\delph\OneDrive\Documents\\UCL\B3Q2\LINFO1341 - Computer networks  information transfer\Projet\projet_reseaux_1\paquets\mess\envoie_recois_mess_4G\paquet1.pcapng"
+
+print(get_TCP_dest(filepath))
